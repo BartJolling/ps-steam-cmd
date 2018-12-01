aea3c0842ce29987e59595864dd6dc1f17406e19
BartJolling <BartJolling@users.noreply.github.com>
2018-12-01 16:50:57 +0100

    Exec SteamCmd in script folder itself and avoid throw

--- SteamCmd.Tests.ps1
+++ SteamCmd.Tests.ps1
@@ -1,25 +1,30 @@
 Using module .\SteamCmd.psm1;
 
-Describe 'SteamCmd Constructor' {
+Describe 'SteamCmd' {
+
+    $scriptFolder = Split-Path -Parent $PSCommandPath;
+    $steamFolder = Join-Path $scriptFolder '\steamcmd\';
 
     BeforeEach {
-        $Script:steamcmd = [SteamCmd]::new('.\steamcmd\', $true);
+        $Script:steamcmd = [SteamCmd]::new( $steamFolder, $true);
     };
 
     Context ': Constructor' {
         It 'set the installation folder for steamcmd' {
-            $steamCmd._Steamcmdfolder.ToString() | Should Be '.\steamcmd\';
+            $steamCmd._Steamcmdfolder.FullName | Should Be $steamFolder;
         }
     };
 
     Context ': GetSteamcmdExePath' {
         It 'returns a valid path to steamcmd.exe' {
-            $steamcmdPath = $steamcmd.GetSteamcmdExePath();
-            Test-Path -Path $steamcmdPath | Should Be $true;
+            { 
+                $steamcmdPath = $steamcmd.GetSteamcmdExePath() 
+                Test-Path -Path $steamcmdPath | Should Be $true;
+            } | Should Not Throw;            
         }        
     }
-
-    Context ': ' {
+    
+    Context ': CleanAppInfoPrint' {
         It 'extracts a valid vdf from the console output of steamcmd' {
             $appInfoContent = Get-Content -Path ./vdf-test-files/appInfoPrint258550.txt -Raw;
             $appInfoContent = $steamcmd.CleanAppInfoPrint("258550", $appInfoContent);
